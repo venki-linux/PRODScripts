3d7a85951a29eeb0bbae95618a18cd5db2890941
venki-linux <venkatchinna345@gmail.com>
2023-05-22 11:54:16 +0530

    update1.0

---
+++ add.sh
@@ -0,0 +1,3 @@
+echo "Adding two numbers using arguments"
+c=`expr $1 + $2`
+echo "Value of c= $c"
---
+++ script-arg.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Script Arguments List"
+echo "\$* Value= $*"
+echo "\$@ Value= $@"
+echo "\$0 Value= $0"
+echo "\$1 Value= $1"
+echo "\$2 Value= $2"
+echo "\$3 Value= $3"
+echo "\$4 Value= $4"
+echo "\$5 Value= $5"
+echo "\$# Value= $#  Total Arguments count"
+echo "\$$ Value= $$"
